2699081bb7c9d11f608caaa62a360988b835f075
Bharath-Parthipan <bharathps00624@gmail.com>
2024-08-16 20:22:50 +0530

    feat:  uplode api updated

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,6 +1,58 @@
-const express = require('express');
-const path = require('path');
-const multer = require('multer');
+// const express = require('express');
+// const path = require('path');
+// const multer = require('multer');
+
+// const router = express.Router();
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+
+//   filename: (req, file, cb) => {
+//     const extname = path.extname(file.originalname);
+//     cb(null, `${file.fieldname}-${Date.now()}${extname}`);
+//   },
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   const filetypes = /jpe?g|png|webp/;
+//   const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
+//   const extname = path.extname(file.originalname).toLowerCase();
+//   const mimetype = file.mimetype;
+
+//   if (filetypes.test(extname) && mimetypes.test(mimetype)) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Images only"), false);
+//   }
+// };
+
+// const upload = multer({ storage, fileFilter });
+// const uploadSingleImage = upload.single("image");
+
+// router.post("/", (req, res) => {
+//   uploadSingleImage(req, res, (err) => {
+//     if (err) {
+//       res.status(400).send({ message: err.message });
+//     } else if (req.file) {
+//       res.status(200).send({
+//         message: "Image uploaded successfully",
+//         image: `/${req.file.path}`,
+//       });
+//     } else {
+//       res.status(400).send({ message: "No image file provided" });
+//     }
+//   });
+// });
+
+// module.exports = router;
+
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+const fs = require("fs");
 
 const router = express.Router();
 
@@ -37,9 +89,19 @@ router.post("/", (req, res) => {
     if (err) {
       res.status(400).send({ message: err.message });
     } else if (req.file) {
-      res.status(200).send({
-        message: "Image uploaded successfully",
-        image: `/${req.file.path}`,
+      // Read the file as a buffer
+      fs.readFile(req.file.path, (err, data) => {
+        if (err) {
+          return res.status(500).send({ message: "Error reading the file" });
+        }
+
+        // Convert the buffer to a base64 string
+        const base64Image = data.toString("base64");
+
+        res.status(200).send({
+          message: "Image uploaded successfully",
+          image: base64Image,
+        });
       });
     } else {
       res.status(400).send({ message: "No image file provided" });
