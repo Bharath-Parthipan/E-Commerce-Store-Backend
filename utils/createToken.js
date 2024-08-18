1c1b68c65178246818ac3ec8461bebd678bb9cf5
Bharath-Parthipan <bharathps00624@gmail.com>
2024-08-18 20:40:53 +0530

    updated the jwt token generation

--- utils/createToken.js
+++ utils/createToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
     // set JWT as an HTTP-Only cookie
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "strict",
         maxAge: 30 * 24 * 60 * 60 * 1000,
     });
